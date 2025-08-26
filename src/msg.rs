0e948b05601d0540d60273417fb4c995ef5a055b
fujis <ppdx999@gmail.com>
2025-08-26 21:32:13 +0900

    remove dead code

--- src/msg.rs
+++
@@ -1,8 +0,0 @@
-/// Messages the root understands.
-pub enum AppMsg {
-    Quit,
-    // ConnectionSelected(ConfigConnection),
-    // Dashboard(DashboardMsg), // mapped child
-    Error(String),
-    // ... async completions, timers, etc.
-}
